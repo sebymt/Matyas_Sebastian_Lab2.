e278239884af3ab6caa5452f13a0dd66ee82e9c6
SEB <SEB@VERSACE>
2021-10-16 16:47:34 +0300

    Add project files.

---
+++ Data/LibraryContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Matyas_Sebastian_Lab2.Models;
+
+namespace Matyas_Sebastian_Lab2.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Book> Books { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<Book>().ToTable("Book");
+        }
+    }
+}
