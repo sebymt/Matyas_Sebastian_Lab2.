61fd8e38f76994d7e9df8450247a72a4040039d9
SEB <SEB@VERSACE>
2021-10-21 20:52:04 +0300

    Lab 4

---
+++ Models/PublishedBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Matyas_Sebastian_Lab2.Models
+{
+    public class PublishedBook
+    {
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
+        public Publisher Publisher { get; set; }
+        public Book Book { get; set; }
+    }
+}
